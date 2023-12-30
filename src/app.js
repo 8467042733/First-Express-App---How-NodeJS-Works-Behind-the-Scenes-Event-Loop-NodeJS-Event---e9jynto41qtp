ee58279a1dcd8206e4b49e65b38edf88e75a9de3
8467042733 <rupeshpal25112004@gmail.com>
2023-12-30 21:59:46 +0530

    add some code

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ const port = 3000;
 app.get('', (req, res) => {
   // TODO: Implement handling of the root endpoint '/'
   // Replace this comment with your code to send "Hello World!"
+  res.send("Hello World!");
 });
 
 app.get('', (req, res) => {
   // TODO: Implement handling of the '/about' endpoint
   // Replace this comment with your code to send "Welcome to about page"
+  res.send("Welcome to about page");
 });
 
 app.listen(port, () => {
--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 dotenv.config();
+app.listen(3000,()=>console.log('server running......'));
